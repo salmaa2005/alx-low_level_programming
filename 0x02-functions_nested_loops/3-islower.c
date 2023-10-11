5ae774de882027f3ebd2fbc5ebe51faea832c937
Salma <salmaamostafa2005@gmail.com>
2023-10-10 19:53:21 -0700

    done

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *  * islower - check the code.
+ *   *
+ *    * Return: Always 0.
+*/
+int _islower(int c)
+{
+int c;
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+	return (0);
+}
+}
