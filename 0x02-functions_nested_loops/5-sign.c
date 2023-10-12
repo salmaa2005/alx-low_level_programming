428f261ba00be91370736cd0c4134ca640951208
Salma <salmaamostafa2005@gmail.com>
2023-10-12 08:09:43 -0700

    done

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * _sign - checks the sign of the integer
+ * @n: it's the integer to be checked
+ *
+ * Return: returns 1 if it's positive , returns 0 if it's 0 and -1 if it's negative
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n = 0)
+		return (0);
+	return (1);
+}	
