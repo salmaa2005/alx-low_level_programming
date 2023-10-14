2bd0f8ccc73a108d39e1e13efdb1840d90d18741
Salma <salmaamostafa2005@gmail.com>
2023-10-13 20:53:47 -0700

    done

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagonal - prints slashes like a diagonal
+ * @n: the number of slashes to be printed
+ *
+ * Return: returns void
+ */
+void print_diagonal(int n)
+{
+	int x, spaces;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (x = 1; x <= n; x++)
+	{
+		for (spaces = 0; spaces < x; spaces++)
+			_putchar(' ');
+		_putchar(92);
+		_putchar('\n');
+	}
+}
