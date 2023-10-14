0086698864871ca749a6e1cfd2fc59f0fa0ac8da
Salma <salmaamostafa2005@gmail.com>
2023-10-13 18:16:54 -0700

    done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 1 to 9
+ * @void
+ *
+ * Return: returns void
+ */
+void print_numbers(void)
+{
+	int i;
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
