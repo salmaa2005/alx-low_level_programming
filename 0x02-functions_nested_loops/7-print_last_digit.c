903561700f58c059027e6a6e1ac786936d25bdb8
Salma <salmaamostafa2005@gmail.com>
2023-10-15 11:30:12 -0700

    done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,15 +8,14 @@
  */
 int print_last_digit(int n)
 {
-
-	if (n >= 0)
+	if (n < 0)
 	{
-		_putchar((n % 10) + 48);
-		return (n % 10);
+		n = (n % 10) * -1;
 	}
 	else
 	{
-		_putchar((-n % 10) + 48);
-		return ((-n) % 10);
+		n = n % 10;
 	}
+	_putchar (n + 48);
+	return (n);
 }
