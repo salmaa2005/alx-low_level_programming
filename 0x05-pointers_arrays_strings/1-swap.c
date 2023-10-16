fbdf5d3ce4842db1e8d5750b4a85726a0f9ac9b4
Salma <salmaamostafa2005@gmail.com>
2023-10-16 14:36:54 -0700

    done

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_most_numbers - prints the numbers except 2 , 4
- * @void
+ * swap_int - swaps the values of two integers
+ * @a: the integer to be swapped
+ * @b: the other integer
  *
  * Return: returns void
  */
 void swap_int(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
