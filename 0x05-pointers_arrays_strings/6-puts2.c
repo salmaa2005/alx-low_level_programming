067b49386328c78fb48a4455b816848db2b2e15a
Salma <salmaamostafa2005@gmail.com>
2023-10-16 19:02:54 -0700

    done

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string, followed by a new lin
+ * @str: a pointer that points to an array of characters
+ *
+ * Return: returns void
+ */
+void puts2(char *str)
+{
+	int i, length;
+
+	length = 0;
+
+	while (str[length] != '\0')
+		length++;
+	for (i = 0; i <= length - 1; i = i + 2)
+		_putchar(str[i]);
+}
