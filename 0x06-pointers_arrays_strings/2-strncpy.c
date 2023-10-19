6fd283c23ae3e52f6c8e84c2b67970c7cd767a26
Salma <salmaamostafa2005@gmail.com>
2023-10-19 16:07:50 -0700

    done

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,12 +12,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		if (src[i] != '\0')
-			dest[i] = src[i];
-		else
-			break;
+		for (i = 0; i < n; i++)
+		{
+			if (src[i] != '\0')
+				dest[i] = src[i];
+			else
+				break;
+		}
 	}
 	return (dest);
 }
