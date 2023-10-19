37005bfdcaa2ced85f6858e81142059e7f6e8bbd
Salma <salmaamostafa2005@gmail.com>
2023-10-19 15:03:22 -0700

    done

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strncpy - copies a string of a specific number of characters
+ * @src: a pointer that points to an array of characters
+ * @dest: a pointer that points to an array of characters
+ * @n: the number of characters to be written
+ *
+ * Return: returns a pointer to an array of characters
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (src[i] != '\0')
+			dest[i] = src[i];
+		else
+			dest[i] = '\0';
+	}
+	return (dest);
+}
