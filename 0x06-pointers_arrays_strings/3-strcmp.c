7f020207e1bed121856f8f63267cbf36ab9715e9
Salma <salmaamostafa2005@gmail.com>
2023-10-19 18:13:19 -0700

    done

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strncpy - copies a string to a certain destination with a specific number of characters
+ * @src: a pointer that points to an array of characters
+ * @dest: a pointer that points to an array of characters
+ *
+ * Return: returns a pointer to an array of characters
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, j;
+
+	for (s1_len = 0; s1[s1_len]; s1_len++)
+		;
+	for (s2_len = 0; s2[s2_len]; s2_len++)
+		;
+	if (i < s1_len && i < s2_len)
+	{
+		for (i = 0;;i++)
+			
+	}
+}
