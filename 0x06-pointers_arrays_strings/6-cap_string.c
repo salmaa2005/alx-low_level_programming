b2863a2db9f7413677eb3c6dacfc1d889cf73add
Salma <salmaamostafa2005@gmail.com>
2023-10-20 17:11:17 -0700

    done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,8 +12,8 @@ char *cap_string(char *a)
 
 	for (i = 0; a[i] != '\0'; i++)
 	{
-		if (i = 0 && a[i + 1] <= 122 && a[i + 1] >= 97)
-			a[i + 1] -= 32;
+		if (i = 0)
+			a[i] -= 32;
 		switch (a[i])
 		{
 			case ' ':
