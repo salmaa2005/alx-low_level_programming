c15075e9fd1ff56843407fc5cbf7da23f67e82a0
Salma <salmaamostafa2005@gmail.com>
2023-10-20 18:34:07 -0700

    done

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,7 +6,19 @@
  *
  * Return: returns a pointer to an array of characters
  */
-char *leet(char *)
+char *leet(char *s)
 {
+	int i, j;
+	char check[5] = {'A', 'E', 'O', 'T', 'L'};
+	int nums[5] = {'4', '3', '0', '7', '1'};
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == check[j] || s[i] == check[j] + 32)
+				s[i] = nums[j];
+		}
+	}
+	return (s);
 }
