321154fc37e15c931fbc956f46b3127f761472bc
Salma <salmaamostafa2005@gmail.com>
2023-10-28 05:39:18 -0700

    done

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _strncpy - copies a string to a certain destination with a specific number of characters
- * @src: a pointer that points to an array of characters
- * @dest: a pointer that points to an array of characters
- *
+ * _memset - fills memory with a constant byte
+ * @s: a pointer that points to an array of characters
+ * @b: the character to replace
+ * @n: an integer that determines the replaced indeces
  * Return: returns a pointer to an array of characters
  */
 char *_memset(char *s, char b, unsigned int n)
