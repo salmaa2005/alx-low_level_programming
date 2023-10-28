495b949599045e57c1bcafae1bff3a004dc03c52
Salma <salmaamostafa2005@gmail.com>
2023-10-28 12:24:29 -0700

    done

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,8 @@
+/* _strpbrk - searches a string for any of a set of bytes
+ *
+ * @s: input string
+ * @accept: input substring
+ *
+ * Return: pointer to the byte in s that matches one of the bytes in accept
+ * or NULL if no such byte is found
+ */
