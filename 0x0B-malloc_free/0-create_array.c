03a368f871b73d600d0d24e84f0b42be294e554e
Salma <salmaamostafa2005@gmail.com>
2023-10-31 08:28:15 -0700

    done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,9 +14,11 @@ char *create_array(unsigned int size, char c)
 	int i, len = 0;
 	char *ptr;
 
-	ptr = (char *)malloc(size);
+	ptr = (char *)malloc(size * sizeof(char));
 	while (ptr[len] != '\0')
 		len++;
+	if (size == 0)
+		return ('\0');
 	for (i = 0; i < len; i++)
 		*(ptr + i) = c;
 	return (ptr);
