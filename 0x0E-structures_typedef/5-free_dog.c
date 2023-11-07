08ea59ffe2871bc030e914edd4a560e07f4fd1bd
Salma <salmaamostafa2005@gmail.com>
2023-11-07 10:22:01 -0800

    done

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "dog.h"
 #include <stdlib.h>
 /**
 * free_dog - frees the allocated memory
@@ -16,4 +16,19 @@ void free_dog(dog_t *d)
 			free(d->owner);
 		free(d);
 	}
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    dog_t *my_dog;
+
+    my_dog = new_dog("Poppy", 3.5, "Bob");
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+    free_dog(my_dog);
+    return (0);
 }
