ca997e5d261e3b1d79de98f21b51d34ec8174b01
Salma <salmaamostafa2005@gmail.com>
2023-11-08 18:36:44 -0800

    done

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+*  - 
+* @
+* @
+* @
+*
+* Return: returns a pointer
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int *end = array + size - 1;
+
+	if (array && size && action)
+		while (array <= end)
+			action(*array++);
+}
