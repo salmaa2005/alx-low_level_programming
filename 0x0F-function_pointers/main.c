150e555eebc4bc0b1319b6918fe93ba2e7967fee
Salma <salmaamostafa2005@gmail.com>
2023-11-08 19:59:47 -0800

    done

---
+++ 0x0F-function_pointers/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+*  - 
+* @
+* @
+* @
+*
+* Return: returns a pointer
+*/
+int main(int argc, char **argv)
+{
+	int (*op_func(int, int), a, b;
+
+	if (argc != 4)
+		printf("Error\n"), exit(98);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+}
