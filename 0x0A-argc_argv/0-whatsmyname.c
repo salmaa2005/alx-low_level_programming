b60a51f1576df082cd2ba361063cec833334e7dc
salmaa2005 <salmaamostafa2005@gmail.com>
2023-11-14 22:36:41 +0200

    fix betty style

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,12 @@
+#include "main.h"
 #include <stdio.h>
-
 /**
-* main - prints its name, followed by a new line
-* @argc: argument count
-* @argv: argument vector
-*
-* Return: returns void
-*/
+ * main - prints its name, followed by a new line
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: returns void
+ */
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
