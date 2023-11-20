90b287f5988b2dd536016ac28a694efb21dbc5fa
salmaa2005 <salmaamostafa2005@gmail.com>
2023-11-20 23:56:55 +0200

    fix print_list 0.5

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,16 +3,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes
+ */
 size_t print_list(const list_t *h)
 {
-	int nodes = 0;
+	size_t nodes = 0;
 
 	if (h == NULL)
 	{
 		return (0);
 	}
 
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
 		{
