6f6df8d44d9caf6a6dc0699a589b17030a554883
salmaa2005 <salmaamostafa2005@gmail.com>
2024-01-17 22:17:50 +0200

    add 2 functions

---
+++ 0x18-dynamic_libraries/putchar.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to be printed
+ *
+ * Return: if successful retiurns 1, otherwise -1
+ */
+int _putchar(char c) { return (write(1, &c, 1)); }
