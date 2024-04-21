c83b0406b208609301891657ae3810bf7de44129
salmaa2005 <salmaamostafa2005@gmail.com>
2024-04-21 13:31:01 +0200

    add times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ * Return: void
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar(i * j + '0');
+		}
+	}
+}
