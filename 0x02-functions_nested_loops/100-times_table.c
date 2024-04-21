9e64726a108a406e098aaed9d48d88cb288a19cf
salmaa2005 <salmaamostafa2005@gmail.com>
2024-04-21 16:02:11 +0200

    fix 100-times_table.c v0.9

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,26 +7,42 @@
  */
 void print_times_table(int n)
 {
-	int i, j;
+	int i, j, res;
 
-	if (n > 15 || n < 0)
-		return;
-	for (i = 0; i <= n; i++)
+	if (!(n > 15 || n < 0))
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			printf("%d", i * j);
-			if (j == n)
-				continue;
-
-			if ((i * j) < 10)
-				printf(",   ");
-			else if ((i * j) < 100)
-				printf(",  ");
-			else
-				printf(", ");
+			for (j = 0; j <= n; j++)
+			{
+				res = (i * j);
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (res < 10 && j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 10 && res < 100)
+				{
+					_putchar(' ');
+					_putchar((res / 10) + '0');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 100 && j != 0)
+				{
+					_putchar((res / 100) + '0');
+					_putchar((res / 10) % 10 + '0');
+					_putchar((res % 10) + '0');
+				}
+				else
+					_putchar((res % 10) + '0');
+			}
+			_putchar('\n');
 		}
-
-		printf("\n");
 	}
 }
