ee04a0dba7c3f47d575fc4c943179f7a0bcf2068
salmaa2005 <salmaamostafa2005@gmail.com>
2024-04-21 15:09:12 +0200

    fix 98 battery street v0.6

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -27,19 +27,24 @@ void print_to_98(int n)
 	}
 	else
 	{
+		for (i = n; i >= 98; i--)
 		{
-			for (i = n; i >= 98; i--)
-				if (n < 100)
-				{
-					_putchar('0' + (i / 10));
-					_putchar('0' + (i % 10));
-				}
-				else
-				{
-					_putchar('0' + (i / 100));
-					_putchar('0' + (i / 10) % 10);
-					_putchar('0' + (i % 10));
-				}
+			if (n < 100)
+			{
+				_putchar('0' + (i / 10));
+				_putchar('0' + (i % 10));
+			}
+			else
+			{
+				_putchar('0' + (i / 100));
+				_putchar('0' + (i / 10) % 10);
+				_putchar('0' + (i % 10));
+			}
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 	}
 	_putchar('\n');
