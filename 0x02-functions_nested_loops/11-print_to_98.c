ec40a190715382e1c80e43daedb088f0b73da355
salmaa2005 <salmaamostafa2005@gmail.com>
2024-04-21 14:29:41 +0200

    add 98 battery street

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: the number
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+	for (n; n <= 98; n++)
+	{
+		if (n < 10)
+			_putchar('0' + n);
+		else
+		{
+			_putchar('0' + (n / 10));
+			_putchar('0' + (n % 10));
+		}
+		if (n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
