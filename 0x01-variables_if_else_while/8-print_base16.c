f60f6d1688f2f59f85ae7341aa3e97942dea22c2
salmaa2005 <salmaamostafa2005@gmail.com>
2024-04-21 11:09:43 +0200

    add ninth task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	for (char c = 'a'; c <= 'f'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
