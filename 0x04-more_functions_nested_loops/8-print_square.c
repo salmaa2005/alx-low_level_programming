f4ebbc9febfae4991866f131af38d27259799b72
salmaa2005 <salmaamostafa2005@gmail.com>
2024-04-22 12:01:12 +0200

    fix 8-print_square.c V0.1

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,10 +9,18 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 0; i < size; i++)
+	if (size > 0)
 	{
-		for (j = 0; j < size; j++)
-			printf("#");
-		printf("\n");
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				printf("#");
+			printf("\n");
+		}
 	}
+}
+int main(void)
+{
+	print_square(2);
+	print_square(10);
 }
