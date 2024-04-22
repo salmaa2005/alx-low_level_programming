cad30efb8b268a0ee2b8a133dd60ae569c29dcda
salmaa2005 <salmaamostafa2005@gmail.com>
2024-04-22 11:30:40 +0200

    fix 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,17 +7,15 @@
 int main(void)
 {
 	int i;
-	long arr[50], sum = 0;
+	long arr[32], sum = 0;
 
 	arr[0] = 1;
 	arr[1] = 2;
-	for (i = 2; i < 50; i++)
+	for (i = 2; i < 32; i++)
 	{
 		arr[i] = arr[i - 1] + arr[i - 2];
-		if (arr[i] > 4000000)
-			break;
 	}
-	for (i = 0; i < 49; i++)
+	for (i = 0; i < 32; i++)
 	{
 		if (arr[i] % 2 == 0)
 			sum += arr[i];
