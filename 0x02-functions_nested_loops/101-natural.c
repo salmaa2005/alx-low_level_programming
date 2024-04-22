1b3bdad8944223ef202ee6ff27f53cf49e29e7cd
salmaa2005 <salmaamostafa2005@gmail.com>
2024-04-22 11:00:45 +0200

    fix 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,11 +6,13 @@
  */
 int main(void)
 {
-	int i;
+	int i, sum = 0;
+
 	for (i = 0; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
-			printf("%d\n", i);
+			sum += i;
 	}
+	printf("%d\n", sum);
 	return (0);
 }
